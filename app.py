a78627552583fba8d20d0fd48cbd109c7054c524
Guilherme Tomaselli Borchardt <99049128+guitomaselli07@users.noreply.github.com>
2023-01-06 21:20:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def grafico_estudantes(escolha_NOME_IES, escolha_IES, escolha_NOME_CURSO, escolh
   if(len(escolha_GRAFICOS) == 0):
     st.subheader('')
     st.error('É necessária a escolha de pelo menos uma opção de gráfico. Por favor, tente novamente.')
-  else:
+  if(len(escolha_GRAFICOS) >= 1):
     
     anos = [2019, 2020, 2021]
       
