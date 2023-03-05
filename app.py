482a61c7082f5c99ef4c9a0f41bc15fc867dae6e
Guilherme Tomaselli Borchardt <99049128+guitomaselli07@users.noreply.github.com>
2023-03-05 10:46:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -561,8 +561,8 @@ def pagina_inicial(dados):
     escolha_IES = int(dados[(dados['NU_ANO_CENSO'] >= 2015) & (dados['CO_UF'] == escolha_ESTADO) & (dados['SG_IES'] == escolha_SG_IES)]['CO_IES'].drop_duplicates())
     escolha_CURSO = st.sidebar.selectbox('Escolha um curso:', (dados[(dados['NU_ANO_CENSO'] >= 2015) & (dados['CO_UF'] == escolha_ESTADO) & (dados['SG_IES'] == escolha_SG_IES)]['NOME_CURSO'].drop_duplicates().sort_values().dropna()))
     if(int((dados[(dados['CO_UF'] == escolha_ESTADO) & (dados['SG_IES'] == escolha_SG_IES) & (dados['NOME_CURSO'] == escolha_CURSO)]['QT_CONC'].sum())) > 0):
-      if(dados[(dados['CO_UF'] == escolha_ESTADO) & (dados['SG_IES'] == escolha_SG_IES) & (dados['NOME_CURSO'] == escolha_CURSO) & (dados['NU_ANO_CENSO'] == 2019)]['QT_MAT'].sum() > 0 and dados[(dados['CO_UF'] == escolha_ESTADO) & (dados['SG_IES'] == escolha_SG_IES) & (dados['NOME_CURSO'] == escolha_CURSO) & (dados['NU_ANO_CENSO'] == 2020)]['QT_MAT'].sum() > 0 and dados[(dados['CO_UF'] == escolha_ESTADO) & (dados['SG_IES'] == escolha_SG_IES) & (dados['NOME_CURSO'] == escolha_CURSO) & (dados['NU_ANO_CENSO'] == 2021)]['QT_MAT'].sum() > 0):
-        escolha_CATEGORIA = st.sidebar.selectbox('Escolha o que deseja analisar:', ('Concluintes', 'Evadidos', 'Ingressantes', 'Matriculados'))
+      if(dados[(dados['CO_UF'] == escolha_ESTADO) & (dados['SG_IES'] == escolha_SG_IES) & (dados['NOME_CURSO'] == escolha_CURSO) & (dados['QT_MAT'] > 0)]['NU_ANO_CENSO'].count() >= 4):
+      escolha_CATEGORIA = st.sidebar.selectbox('Escolha o que deseja analisar:', ('Concluintes', 'Evadidos', 'Ingressantes', 'Matriculados'))
         if(escolha_CATEGORIA == 'Evadidos'):
           button_gerar_grafico = st.sidebar.button('Gerar Gráfico')
           if(button_gerar_grafico):
